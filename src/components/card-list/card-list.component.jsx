177656059f1fdf1054d9f13b752411f6ca195312
kammielatay <kameshialatay@gmail.com>
2022-09-01 20:16:38 -0700

    updating code to functional react

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,35 @@
-import { Component } from "react";
-import Card from '../card/card.component'
+//import { Component } from "react";
 
-import './card-list.styles.css'
+import Card from "../card/card.component";
 
-class CardList extends Component {
-  
-  render() {
-    const { monsters } = this.props
-    return (
-      <div className='card-list'>
-          {monsters.map((monster) => {
-            return (
-              <Card monster={monster} />
-          )})}
-      </div>
-    )
-  }
-}
+import "./card-list.styles.css";
+
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster, id) => {
+      return <Card monster={monster} key={id} />;
+    })}
+  </div>
+);
+
+// import { Component } from "react";
+//import Card from "../card/card.component";
+
+// import './card-list.styles.css'
+
+// class CardList extends Component {
+
+//   render() {
+//     const { monsters } = this.props
+//     return (
+//       <div className='card-list'>
+//           {monsters.map((monster) => {
+//             return (
+//               <Card monster={monster} />
+//           )})}
+//       </div>
+//     )
+//   }
+// }
 
 export default CardList;
